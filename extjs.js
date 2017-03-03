e68035487ceb704bad6514dbd305c392f54f055c
SimonLoir <simon-loir@hotmail.com>
2017-03-03 22:07:45 +0100

    mise en forme du document et suppression AR (old)

--- extjs.js
+++ extjs.js
@@ -1,61 +1,61 @@
-function $(e, index){
-    return new ExtJsObject(e, index);
+function $(e, index) {
+	return new ExtJsObject(e, index);
 }
 
-function ExtJsObject(element, e_index){
-    var re;
+function ExtJsObject(element, e_index) {
+	var re;
 
-	if (typeof(element) === "string") {
-        re = document.querySelectorAll(element);
-		if(e_index != undefined){
-            re = [re[e_index]];
-        }
-	}else if(element == undefined || element == document){
+	if (typeof (element) === "string") {
+		re = document.querySelectorAll(element);
+		if (e_index != undefined) {
+			re = [re[e_index]];
+		}
+	} else if (element == undefined || element == document) {
         /**
 		 * @param {Function} toDo function that is called when the document has been loaded
 		 */
-		this.ready = function(toDo){
+		this.ready = function (toDo) {
 			document.addEventListener("DOMContentLoaded", toDo);
 		}
 		return this;
-	}else if(typeof(element) === "object"){
-        if(element.length == undefined){
-            re = [element];
-        }else if(e_index != undefined){
-            re = [element[e_index]];
-        }else{
-            re = element;
-        }
-	}else if(element.type == "ExtJsObject"){
+	} else if (typeof (element) === "object") {
+		if (element.length == undefined) {
+			re = [element];
+		} else if (e_index != undefined) {
+			re = [element[e_index]];
+		} else {
+			re = element;
+		}
+	} else if (element.type == "ExtJsObject") {
 		return element;
-	}else{
+	} else {
 		return false;
 	}
 
 	this.type = "ExtJsObject";
-	this.node = re; 
+	this.node = re;
 
     /**
      * @param {String} html HTML to put inside the element or undefined or nothing
      * @return {String|Object} HTML that is inside the first element or the current instance.
      */
-    this.html = function(html){
-        for (var i = 0; i < this.node.length; i++) {
-            var e = this.node[i];
-            
-            if (typeof(html) === "string" || typeof(html) === "number") {
-                e.innerHTML = html;
-            }else{
-                return e.innerHTML;
-            }
-        }
-        return this;
+	this.html = function (html) {
+		for (var i = 0; i < this.node.length; i++) {
+			var e = this.node[i];
+
+			if (typeof (html) === "string" || typeof (html) === "number") {
+				e.innerHTML = html;
+			} else {
+				return e.innerHTML;
+			}
+		}
+		return this;
 	}
 }
 
 var AR = new ExtJsAjaxRequestObject();
 
-function ExtJsAjaxRequestObject(){
+function ExtJsAjaxRequestObject() {
 
 	/**
      * @param {String} url URL of the resource
@@ -64,23 +64,23 @@ function ExtJsAjaxRequestObject(){
      */
 	this.GET = function (url, func, error) {
 		var xhttp = new XMLHttpRequest();
-  		xhttp.onreadystatechange = function() {
-    		if (xhttp.readyState == 4 && xhttp.status == 200) {
-    			func(xhttp.responseText);
-    		} else if (xhttp.readyState == 4) {
+		xhttp.onreadystatechange = function () {
+			if (xhttp.readyState == 4 && xhttp.status == 200) {
+				func(xhttp.responseText);
+			} else if (xhttp.readyState == 4) {
 
-    		    if (error != undefined) {
-    		        try {
-    		            error();
-    		        } catch (e) {
+				if (error != undefined) {
+					try {
+						error();
+					} catch (e) {
 
-    		        }
-    		    }
+					}
+				}
 
-    		}
-    	}
-  		xhttp.open("GET", url, true);
-  		xhttp.send();
+			}
+		}
+		xhttp.open("GET", url, true);
+		xhttp.send();
 	}
     /**
      * @param {String} url URL of the resource
@@ -90,25 +90,25 @@ function ExtJsAjaxRequestObject(){
 	this.DELETE = function (url, func, error) {
 		var xhttp = new XMLHttpRequest();
 
-  		xhttp.onreadystatechange = function() {
-    		if (xhttp.readyState == 4 && xhttp.status == 200) {
-    			func(xhttp.responseText);
-    		} else if (xhttp.readyState == 4) {
+		xhttp.onreadystatechange = function () {
+			if (xhttp.readyState == 4 && xhttp.status == 200) {
+				func(xhttp.responseText);
+			} else if (xhttp.readyState == 4) {
 
-    		    if (error != undefined) {
-    		        try {
-    		            error();
-    		        } catch (e) {
+				if (error != undefined) {
+					try {
+						error();
+					} catch (e) {
 
-    		        }
-    		    }
+					}
+				}
 
-    		}
-    	}
-  		xhttp.open("GET", url, true);
-  		xhttp.setRequestHeader("x-http-method-override", "DELETE");
+			}
+		}
+		xhttp.open("GET", url, true);
+		xhttp.setRequestHeader("x-http-method-override", "DELETE");
 
-  		xhttp.send();
+		xhttp.send();
 	}
 	/**
      * @param {String} url URL of the resource
@@ -116,34 +116,34 @@ function ExtJsAjaxRequestObject(){
      * @param {Function} func function which is called when the request has been performed correctly
      * @param {Function} error function which is called when the request has not been performed correctly
      */
-	this.POST = function (url, data ,func, error) {
+	this.POST = function (url, data, func, error) {
 		var xhttp = new XMLHttpRequest();
-  		xhttp.onreadystatechange = function() {
-  		    if (xhttp.readyState == 4 && xhttp.status == 200) {
-  		        func(xhttp.responseText);
-  		    } else if (xhttp.readyState == 4) {
+		xhttp.onreadystatechange = function () {
+			if (xhttp.readyState == 4 && xhttp.status == 200) {
+				func(xhttp.responseText);
+			} else if (xhttp.readyState == 4) {
 
-  		        if (error != undefined) {
-  		            try {
-  		                error();
-  		            } catch (e) {
+				if (error != undefined) {
+					try {
+						error();
+					} catch (e) {
 
-  		            }
-  		        }
+					}
+				}
 
-            }
-    	}
-  		xhttp.open("POST", url, true);
-  		var keys = 	Object.keys(data);
-  		var d = "";
-  		for (var i = 0; i < keys.length; i++) {
-  			 if (i !== 0 ) {
-  			 	d = d + "&";
-  			 }
-  			 d = d + keys[i] + "=" + data[keys[i]];
-  		}
-  		xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  		xhttp.send(d);
+			}
+		}
+		xhttp.open("POST", url, true);
+		var keys = Object.keys(data);
+		var d = "";
+		for (var i = 0; i < keys.length; i++) {
+			if (i !== 0) {
+				d = d + "&";
+			}
+			d = d + keys[i] + "=" + data[keys[i]];
+		}
+		xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xhttp.send(d);
 	}
     /**
      * @param {String} url URL of the resource
@@ -151,34 +151,34 @@ function ExtJsAjaxRequestObject(){
      * @param {Function} func function which is called when the request has been performed correctly
      * @param {Function} error function which is called when the request has not been performed correctly
      */
-	this.PUT = function (url, data ,func, error) {
+	this.PUT = function (url, data, func, error) {
 		var xhttp = new XMLHttpRequest();
-  		xhttp.onreadystatechange = function() {
-  		    if (xhttp.readyState == 4 && xhttp.status == 200) {
-  		        func(xhttp.responseText);
-  		    } else if (xhttp.readyState == 4) {
+		xhttp.onreadystatechange = function () {
+			if (xhttp.readyState == 4 && xhttp.status == 200) {
+				func(xhttp.responseText);
+			} else if (xhttp.readyState == 4) {
 
-  		        if (error != undefined) {
-  		            try {
-  		                error();
-  		            } catch (e) {
+				if (error != undefined) {
+					try {
+						error();
+					} catch (e) {
 
-  		            }
-  		        }
+					}
+				}
 
-            }
-    	}
-  		xhttp.open("POST", url, true);
-  		var keys = 	Object.keys(data);
-  		var d = "";
-  		for (var i = 0; i < keys.length; i++) {
-  			 if (i !== 0 ) {
-  			 	d = d + "&";
-  			 }
-  			 d = d + keys[i] + "=" + data[keys[i]];
-  		}
-  		xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  		xhttp.setRequestHeader("x-http-method-override", "PUT");
-  		xhttp.send(d);
+			}
+		}
+		xhttp.open("POST", url, true);
+		var keys = Object.keys(data);
+		var d = "";
+		for (var i = 0; i < keys.length; i++) {
+			if (i !== 0) {
+				d = d + "&";
+			}
+			d = d + keys[i] + "=" + data[keys[i]];
+		}
+		xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xhttp.setRequestHeader("x-http-method-override", "PUT");
+		xhttp.send(d);
 	}
 }
