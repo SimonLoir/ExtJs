6f3e90552f872e199806340764d07b8f12cc3a6c
SimonLoir <simon-loir@hotmail.com>
2017-10-14 22:12:22 +0200

    adding the css method

--- extjs-old.js
+++ extjs-old.js
@@ -1,40 +1,39 @@
 function EXTJS_MAIN(element){
 
-	this.css =  function (prop, value) {
-		if (value != undefined) {
-			this.dom.style[prop] = value;
-		}else{
-			return this.dom.style[prop];
-		}
-	}
+    this.css =  function (prop, value) {
+        if (value != undefined) {
+            this.dom.style[prop] = value;
+        }else{
+            return this.dom.style[prop];
+        }
+    }
 
-	this.child =  function(element_type){
-		var elem  = document.createElement(element_type);
-		this.node.appendChild(elem);
-		//console.log($(elem));
-		return $(elem);
-	}
+    this.child =  function(element_type){
+        var elem  = document.createElement(element_type);
+        this.node.appendChild(elem);
+        return $(elem);
+    }
 
-	this.attr = function (attr_name, attr_value) {
-		
-		if (attr_name != undefined) {
-			
-			if (attr_value != undefined) {
-				
-				this.node.setAttribute(attr_name, attr_value);
-				return $(this);
+    this.attr = function (attr_name, attr_value) {
+        
+        if (attr_name != undefined) {
+            
+            if (attr_value != undefined) {
+                
+                this.node.setAttribute(attr_name, attr_value);
+                return $(this);
 
-			}else{
+            }else{
 
-				return this.node.getAttribute(attr_name);
+                return this.node.getAttribute(attr_name);
 
-			}
+            }
 
-		}else{
-			console.error('Fatal error');
-		}
+        }else{
+            console.error('Fatal error');
+        }
 
-	}
-	
-	return this;
+    }
+    
+    return this;
 }
